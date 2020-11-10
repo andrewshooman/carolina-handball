6faad3ea2f9aba928b9ef2907272b2f8772f0dec
AndrewShooman <andrewshooman@gmail.com>
2020-11-09 21:32:28 -0500

    more stuff

---
+++ rl-stats/public/client.js
@@ -0,0 +1,10 @@
+console.log('Client-side code running');
+
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function(e) {
+    console.log("test")
+
+   
+
+})
--- rl-stats/server.js
+++ rl-stats/server.js
@@ -8,30 +8,42 @@ var router = express.Router()
 const PORT = process.env.PORT || 3000;
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:tP92Tbhs8CACl5uM@cluster0.gbvl6.mongodb.net/rl_stats?retryWrites=true&w=majority";
+const uri = "mongodb+srv://host:NGNxDF1XwElvEQ0c@cluster0.gbvl6.mongodb.net/rl_stats?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
 
 
 app.use('/', router);
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/api/hello', (req, res) => {
   res.json('hello world')
 })
 
-router.get('/home',  (req, res) => {
+router.get('/query',  (req, res) => {
   client.connect(err => {
+
     const collection = client.db("rl_stats").collection("player_stats");
 
-    collection.insert('./replay1_data.js')
+    var query = {name:'Bacon'};
+
+    collection.find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
 
     client.close();
   });
 })
+
+// app.get('/', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
+
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
