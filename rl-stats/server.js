1fde0093f1345b5cc7f1d8cabf78c33caa89d544
AndrewShooman <andrewshooman@gmail.com>
2020-11-09 19:03:08 -0500

    more backend shizz

--- rl-stats/server.js
+++ rl-stats/server.js
@@ -5,9 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
 var router = express.Router()
-router.get('/api/hello', (req, res) => {
-  res.json('hello world')
-})
+const PORT = process.env.PORT || 3000;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<tP92Tbhs8CACl5uM>@cluster0.gbvl6.mongodb.net/<rl_stats>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+
 
 app.use('/', router);
 app.use(logger('dev'));
@@ -15,8 +18,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-const PORT = process.env.PORT || 5000;
+
+router.get('/api/hello', (req, res) => {
+  res.json('hello world')
+})
+
+router.get('/home',  (req, res) => {
+  client.connect(err => {
+    const collection = client.db("rl_stats").collection("player_stats");
+
+    collection.insert('./replay1_data.js')
+
+    client.close();
+  });
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-module.exports = app;
