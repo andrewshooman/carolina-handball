996a19bfe1978347dc4189a25f33f6425df85dc1
Bryce Koester <bryce@thekoesters.org>
2020-11-22 18:12:06 -0500

    added favorites page functionality

---
+++ final-project/public/javascripts/favorites.js
@@ -0,0 +1,71 @@
+import countryOfPlayers from "../data/countryOfPlayers.js";
+import currentTeams from "../data/currentTeams.js";
+
+
+let favoritedPlayers = []
+function findCountry(player) {
+    let country = ""
+    console.log(player)
+    for (let i = 1; i < countryOfPlayers.length; i++) {
+        let playerArr = countryOfPlayers[i].players
+        for (let j = 0; j < playerArr.length; j++) {
+            if (playerArr[j].toLowerCase() == player) {
+                country = countryOfPlayers[i];
+                return country
+            }
+        }
+    }
+    return countryOfPlayers[0]
+}
+function findCurrentTeamByPlayer(player) {
+    // let lookie = dataset[0].players.find(p => p.name == player)
+    for (let i = 0; i < currentTeams.length; i++) {
+        if (currentTeams[i].status) {
+            if (currentTeams[i].sub.toLowerCase() == player.toLowerCase() || currentTeams[i].coach.toLowerCase() == player.toLowerCase() ) {
+                return currentTeams[i];
+            }
+            let playerArr = currentTeams[i].players
+            for (let j = 0; j < playerArr.length; j++) {
+                if (playerArr[j].toLowerCase() == player.toLowerCase()) {
+                    return currentTeams[i];
+                }
+            }
+        }
+    }
+    return (currentTeams[0])
+}
+function renderPlayerCard(player) {
+    let country = findCountry(player.name.toLowerCase())
+    let team = findCurrentTeamByPlayer(player.name)
+
+    $('#root').append(
+        `<div id="${player.name}" class="box">
+                <div class="columns is-multiline justify-center">
+                    <div class="column">
+                        <h1 class="title" style="">${player.name}</h1>
+                        <h2 id="${player}Status" class="true">Status: ${team.name != "Not Found" ? "Active" : "Free Agent"}&nbsp<img width="18px" height="18px" src="${team.name != "Not Found" ? "../images/icons/Green Status.jpg" : "../images/icons/Blue Status.jpg"}"></h2>
+                        <h2>Country of Origin: ${country.name}</h2>
+                        <h2>Team: ${team.name}</h2>
+                    </div>
+                    <div class="column">
+                        <span class="is-italic has-text-weight-semibold"></span>
+                        <p></p>
+                    </div>
+                </div>
+            </div>`)
+}
+
+function loadStuffIntoDom() {
+    $.ajax({
+        url: '/secret',
+        type: 'GET',
+        dataType: 'json',
+        success: function (response, textStatus, jqXHR) {
+            console.log(jqXHR.responseJSON);
+            for (let i = 0; i < jqXHR.responseJSON.length; i++) {
+                renderPlayerCard(jqXHR.responseJSON[i]);
+            }
+        }
+    })
+}
+loadStuffIntoDom();
