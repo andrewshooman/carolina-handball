5d6810299f67c38505f87e3ceb1c49d8554945ee
AndrewShooman <andrewshooman@gmail.com>
2021-08-10 20:30:55 -0400

    more changes

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,38 @@
-const container = document.getElementById("testModal");
+const container = document.getElementById("modal1");
+const container2 = document.getElementById("modal2");
+
 const modal = new bootstrap.Modal(container);
+const modal2 = new bootstrap.Modal(container2);
+
 
 document.getElementById("btnShow").addEventListener("click", function () {
   modal.show();
 });
 document.getElementById("btnSave").addEventListener("click", function () {
+  modal.hide();
+});
+document.getElementById("btn-close").addEventListener("click", function () {
+  modal.hide();
+});
+
+
+document.getElementById("btnShow2").addEventListener("click", function () {
+  modal2.show();
+});
+document.getElementById("btnSave2").addEventListener("click", function () {
+  modal2.hide();
+});
+
+document.getElementById("btnShow3").addEventListener("click", function () {
+  modal.show();
+});
+document.getElementById("btnSave3").addEventListener("click", function () {
+  modal.hide();
+});
+
+document.getElementById("btnShow4").addEventListener("click", function () {
+  modal.show();
+});
+document.getElementById("btnSave4").addEventListener("click", function () {
   modal.hide();
 });
